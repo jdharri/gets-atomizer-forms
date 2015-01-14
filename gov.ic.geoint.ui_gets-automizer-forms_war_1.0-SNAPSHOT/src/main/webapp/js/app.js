831ae8cb9da84eb31b0bb33c1ae24b395f619a1f
joel_harris <joel_harris@localhost.localdomain>
2015-01-14 17:35:51 -0500

    added form

---
+++ gov.ic.geoint.ui_gets-automizer-forms_war_1.0-SNAPSHOT/src/main/webapp/js/app.js
@@ -0,0 +1,62 @@
+var softwareDocApp = angular.module('softwareDocApp', ['ngRoute', 'mgcrea.ngStrap', 'formControllers']);
+
+var formControllers = angular.module('formControllers', []);
+
+softwareDocApp.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+                when('/newproposal', {
+                    templateUrl: 'views/projectProposal.html'
+                });
+    }])
+softwareDocApp.controller('NavbarController', function NavbarController($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+
+   
+});
+ softwareDocApp.controller('ActorController', function ActorController($scope) {
+        $scope.actors = [];
+
+        $scope.add = function () {
+           
+            $scope.actors.push({
+            
+                name: "",
+                description: ""
+            });
+        }
+    });
+     softwareDocApp.controller('UseCaseController', function UseCaseController($scope) {
+        $scope.useCases = [];
+
+        $scope.add = function () {
+           
+            $scope.useCases.push({
+                           
+                description: ""
+            });
+        }
+    });
+      softwareDocApp.controller('FunctionalCapabilitiesController', function FunctionalCapabilitiesController($scope) {
+        $scope.functionalCapabilities = [];
+
+        $scope.add = function () {
+           
+            $scope.functionalCapabilities.push({
+                           
+                description: ""
+            });
+        }
+    });
+     softwareDocApp.controller('NonFunctionalCapabilitiesController', function NonFunctionalCapabilitiesController($scope) {
+        $scope.nonFunctionalCapabilities = [];
+
+        $scope.add = function () {
+           
+            $scope.nonFunctionalCapabilities.push({
+                           
+                description: ""
+            });
+        }
+    });
